3e8aa40e3b2858945811ff419055e6c3aba5c497
José Pedro Leão Pereira <zleao@users.noreply.github.com>
2018-04-11 23:08:01 +0200

    Add empty Playground.Core Project

---
+++ Samples/Playground/Playground.Core/App.cs
@@ -0,0 +1,37 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+
+namespace Playground.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Do any UI bound startup actions here
+        /// </summary>
+        /// <param name="hint"></param>
+        public override void Startup(object hint)
+        {
+            base.Startup(hint);
+        }
+
+        /// <summary>
+        /// If the application is restarted (eg primary activity on Android 
+        /// can be restarted) this method will be called before Startup
+        /// is called again
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+        }
+    }
+}
---
+++ Samples/Playground/Playground.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace Playground.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+        private readonly IMvxLogProvider _logProvider;
+        private readonly IMvxViewModelLoader _mvxViewModelLoader;
+
+        public MainViewModel(IMvxNavigationService navigationService, IMvxLogProvider logProvider, IMvxViewModelLoader mvxViewModelLoader)
+        {
+            _navigationService = navigationService;
+            _logProvider = logProvider;
+            _mvxViewModelLoader = mvxViewModelLoader;
+        }
+
+
+    }
+}
