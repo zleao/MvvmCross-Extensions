902d45d3ccd84eb9be9781d871eeee303c494f02
José Pereira <leaopereira@gmail.com>
2018-09-30 19:35:21 +0200

    Add empty ViewControllerBase class for iOS

---
+++ Libraries/MvxExtensions/Platforms/Ios/Views/ViewControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using MvvmCross.Platforms.Ios.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvxExtensions.Platforms.Ios.Views
+{
+    public abstract class ViewControllerBase : MvxViewController
+    {
+    }
+}
