e2466fbecbfd6e47a123b2f77fd6298c7f12309d
José Pereira <leaopereira@gmail.com>
2018-10-02 12:55:25 +0200

    Implement ViewControllerBase for iOS

---
+++ Libraries/MvxExtensions/Platforms/Ios/Components/Interfaces/ILoadingIndicator.cs
@@ -0,0 +1,8 @@
+﻿namespace MvxExtensions.Platforms.iOS.Components.Interfaces
+{
+    public interface ILoadingIndicator
+    {
+        void Show(bool endEditing = true);
+        void Hide();
+    }
+}
