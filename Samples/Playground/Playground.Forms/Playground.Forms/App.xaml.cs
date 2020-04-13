72292ba3b506b070bcc82cffcbcd96b23411a1b4
José Pereira <leaopereira@gmail.com>
2020-04-13 10:59:02 +0200

    Add initial forms sample project with Android, iOS and UWP support

---
+++ Samples/Playground/Playground.Forms/Playground.Forms/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Playground.Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Samples/Playground/Playground.Forms/Playground.Forms/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MvvmCross.Forms.Views;
+using Playground.Core.ViewModels;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Playground.Forms.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MvxContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ItemsCollectionView_SelectionChanged(object sender, Xamarin.Forms.SelectionChangedEventArgs e)
+        {
+            ((MainViewModel)ViewModel).NavigateCommand.Execute(e.CurrentSelection.FirstOrDefault());
+        }
+    }
+}
