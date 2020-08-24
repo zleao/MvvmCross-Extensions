df3d816e2866faf1a316d13dd4fe87887c9f6991
Jose Pereira <leaopereira@gmail.com>
2020-08-24 11:40:44 +0200

    Update Forms Sample (iOS) to MvvmCross v7

--- Samples/Playground/Playground.Forms/Playground.Forms.iOS/AppDelegate.cs
+++ Samples/Playground/Playground.Forms/Playground.Forms.iOS/AppDelegate.cs
@@ -1,31 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
-using UIKit;
+﻿using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
 
 namespace Playground.Forms.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
-    [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<Core.App, App>, Core.App, App>
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
-
-            return base.FinishedLaunching(app, options);
-        }
     }
 }
